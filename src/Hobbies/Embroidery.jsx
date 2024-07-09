f8b55aae0d1440f6f8c2b60e6aea20422d0dbd39
doux124 <h1710055@nushigh.edu.sg>
2024-07-09 20:05:51 +0800

    a

--- src/Hobbies/Embroidery.jsx
+++ src/Hobbies/Embroidery.jsx
@@ -1,21 +1,28 @@
+import { Link } from 'react-router-dom';
+
 const Embroidery = () => {
   return (
-    <div className="container mx-auto p-8">
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
-        <div className="p-4 bg-[#FFECD1] rounded-lg text-white">
-          <img src="/jordan/images/jellyfish.jpg" alt="Steak" className="w-full rounded-lg mb-2" />
-          <h3 className="text-lg font-semibold mb-1 text-[#483C32]">:D</h3>
-          <p className="text-sm text-[#483C32]">Isn't this jellyfish cute</p>
+    <div>
+      <div className="background-image"></div>
+      <div className="hobbies-container p-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+          <div className="p-4 bg-[#FFECD1] rounded-lg text-white">
+            <img src="/jordan/images/jellyfish.jpg" alt="Steak" className="w-full rounded-lg mb-2" />
+            <h3 className="text-lg font-semibold mb-1 text-[#483C32]">Isn't this jellyfish cute</h3>
+            <p className="text-sm text-[#483C32]">made by @potato not me</p>
+          </div>
         </div>
-      </div>
 
-      <div className="flex justify-center mt-8">
-        <a href="#" className="px-4 py-2 bg-blue-500 text-white rounded-lg">«</a>
-        <a href="#" className="px-4 py-2 bg-blue-500 text-white rounded-lg ml-2">1</a>
-        <a href="#" className="px-4 py-2 bg-blue-500 text-white rounded-lg ml-2">2</a>
-        <a href="#" className="px-4 py-2 bg-blue-500 text-white rounded-lg ml-2">3</a>
-        <a href="#" className="px-4 py-2 bg-blue-500 text-white rounded-lg ml-2">4</a>
-        <a href="#" className="px-4 py-2 bg-blue-500 text-white rounded-lg ml-2">»</a>
+        <div className="flex justify-center mt-8">
+          <Link to="/hobbies/origami/" className=" hover-scale px-4 py-2 bg-blue-500 text-white rounded-lg">«</Link>
+          <Link to="/hobbies/origami/" className=" hover-scale px-4 py-2 bg-blue-500 text-white rounded-lg ml-2">1</Link>
+          <div className="hover-scale bg-black ml-2 flex items-center justify-center w-10 h-10 rounded-full">
+            <Link to="/hobbies/embroidery/" className="bg-blue-500 text-white rounded-lg">2</Link>
+          </div>
+          <Link to="/hobbies/origami/" className=" hover-scale px-4 py-2 bg-blue-500 text-white rounded-lg ml-2">3</Link>
+          <Link to="/hobbies/origami/" className=" hover-scale px-4 py-2 bg-blue-500 text-white rounded-lg ml-2">4</Link>
+          <Link to="/hobbies/origami/" className=" hover-scale px-4 py-2 bg-blue-500 text-white rounded-lg ml-2">»</Link>
+        </div>
       </div>
     </div>
   );
--- src/Hobbies/Origami.jsx
+++ src/Hobbies/Origami.jsx
@@ -1,7 +1,31 @@
+import { Link } from 'react-router-dom';
+
 const Origami = () => {
   return (
-    <div>Origami</div>
-  )
+    <div>
+      <div className="background-image"></div>
+      <div className="hobbies-container p-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+          <div className="p-4 bg-[#FFECD1] rounded-lg text-white">
+            <img src="/jordan/images/origami.jpg" alt="Steak" className="w-full rounded-lg mb-2" />
+            <h3 className="text-lg font-semibold mb-1 text-[#483C32]">Squirrel on a Unicorn</h3>
+            <p className="text-sm text-[#483C32]">Took a longg time to fold this</p>
+          </div>
+        </div>
+
+        <div className="flex justify-center mt-8">
+          <Link to="/hobbies/origami/" className="hover-scale px-4 py-2 bg-blue-500 text-white rounded-lg">«</Link>
+          <div className="hover-scale bg-black ml-2 flex items-center justify-center w-10 h-10 rounded-full">
+            <Link to="/hobbies/origami/" className="px-4 py-2 bg-blue-500 text-white rounded-lg">1</Link>
+          </div>
+          <Link to="/hobbies/embroidery/" className="hover-scale px-4 py-2 bg-blue-500 text-white rounded-lg ml-2">2</Link>
+          <Link to="/hobbies/origami/" className="hover-scale px-4 py-2 bg-blue-500 text-white rounded-lg ml-2">3</Link>
+          <Link to="/hobbies/origami/" className="hover-scale px-4 py-2 bg-blue-500 text-white rounded-lg ml-2">4</Link>
+          <Link to="/hobbies/origami/" className="hover-scale px-4 py-2 bg-blue-500 text-white rounded-lg ml-2">»</Link>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Origami
