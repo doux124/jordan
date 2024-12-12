e00ac633353fbb1a3f68822c3b316d4c91352305
doux124 <onezeroten124@gmail.com>
2024-12-12 15:57:18 +0800

    v1.7.3 minor improvements

--- src/hidden/piano/RandomNote.jsx
+++ src/hidden/piano/RandomNote.jsx
@@ -5,6 +5,7 @@ import '../../standards/buttons.css';
 function RandomNote({ setHighlightedNote }) {
   const [selectedNote, setSelectedNote] = useState(null);
   const [player, setPlayer] = useState(null);
+  const [isNoteClicked, setIsNoteClicked] = useState(false);
 
   const notes = [
     'C3', 'C#3', 'D3', 'D#3', 'E3', 'F3', 'F#3', 'G3', 'G#3', 'A3', 'A#3', 'B3'
@@ -18,17 +19,25 @@ function RandomNote({ setHighlightedNote }) {
     const randomNote = notes[Math.floor(Math.random() * notes.length)];
     setSelectedNote(randomNote);
     setHighlightedNote(randomNote);
+    setIsNoteClicked(false); // Reset the click state before playing the note
     if (player) {
       player.play(randomNote);
     }
   };
 
+  const handleClickNote = () => {
+    setIsNoteClicked(true); // Set the state to show the note
+  };
+
   return (
     <div>
-      <div className="flex-center flex-wrap gap-4 p-6">
+      <div className="flex-center flex-wrap gap-4 pt-6 pb-3">
         <button className='button-85' onClick={handlePlayRandomNote}>Play Random Note</button>
       </div>
-      {selectedNote && <p>Played Note: {selectedNote}</p>}
+      {selectedNote && !isNoteClicked && (
+        <p onClick={handleClickNote}>Played Note: ██</p>
+      )}
+      {isNoteClicked && <p>Played Note: {selectedNote}</p>}
     </div>
   );
 }
