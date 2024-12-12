697eeaf5fc4bdd38395224dc09729d1d997bfb5c
doux124 <onezeroten124@gmail.com>
2024-12-12 16:43:46 +0800

    Updates

--- assets/RGBWordInput-DQLG0VGL.js
+++
@@ -1,1 +0,0 @@
-import{r as a,j as t}from"./index-BAFaIpAZ.js";import{R as n}from"./RGBWord-BvzAUtUy.js";const p=()=>{const[e,r]=a.useState(""),s=o=>{r(o.target.value)};return t.jsxs("div",{children:[t.jsx("input",{type:"text",placeholder:"Enter text",value:e,onChange:s,style:{color:"black"}}),t.jsx(n,{text:e})]})};export{p as default};
