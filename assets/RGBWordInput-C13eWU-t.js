7146ebe7da42c11525c449cae8c421a09890292b
doux124 <onezeroten124@gmail.com>
2024-12-13 10:38:33 +0800

    Updates

---
+++ assets/RGBWordInput-C13eWU-t.js
@@ -0,0 +1,1 @@
+import{r as a,j as t}from"./index-Cu2d2rFe.js";import{R as n}from"./RGBWord-DcbgU5-R.js";const p=()=>{const[e,r]=a.useState(""),s=o=>{r(o.target.value)};return t.jsxs("div",{children:[t.jsx("input",{type:"text",placeholder:"Enter text",value:e,onChange:s,style:{color:"black"}}),t.jsx(n,{text:e})]})};export{p as default};
